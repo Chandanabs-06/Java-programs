48527f5bae11bf3abd2d92f64107c95c2328eea7
Chandanabs-06 <chandanabs006@gmail.com>
2026-06-09 17:36:12 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] marks = {85, 90, 78, 88, 95};
+
+        System.out.println("Array Elements:");
+        for(int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
